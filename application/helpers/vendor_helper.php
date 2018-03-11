686de1e59f4642bf70e47a4ea0d6d205a951f670
Ian Escarro <ian.escarro@gmail.com>
2018-03-11 21:14:21 +0800

    Linked vendors to company

--- application/helpers/vendor_helper.php
+++ application/helpers/vendor_helper.php
@@ -1,7 +1,8 @@
 <?php
-function vendor_form() {
+function vendor_form($company_id) {
   $obj = &get_instance();
   return array(
+    'company_id' => $company_id,
     'name' => $obj->input->post('name'),
     'address' => $obj->input->post('address'),
     'phone' => $obj->input->post('phone'),
