78b9cd4f4db097c9949b3a728d27f4ebc43f2a21
Ian Escarro <ian.escarro@gmail.com>
2018-03-14 16:02:34 +0800

    Initial tracking entry

--- application/models/Customer_model.php
+++ application/models/Customer_model.php
@@ -9,6 +9,14 @@ class Customer_model extends CI_Model {
     return $this->db->get_where('customers', array('company_id' => $company_id))->result();
   }
 
+  function find_by_company_for_dropdown($company_id) {
+    $rows = array();
+    foreach ($this->find_by_company($company_id) as $row) {
+      $rows[$row->id] = $row->name;
+    }
+    return $rows;
+  }
+
   function read($id) {
     return $this->db->get_where('customers', array('id' => $id))->row();
   }
