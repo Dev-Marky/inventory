61d320547f5cee9bca28225a65ab61dc04525302
Ian Escarro <ian.escarro@gmail.com>
2018-03-14 15:19:51 +0800

    Fixed base url from fork

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/scoollabs/inventory';
+$config['base_url'] = 'http://localhost:8888/deghq/inventory';
 
 /*
 |--------------------------------------------------------------------------
